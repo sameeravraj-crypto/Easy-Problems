82715a26e03fa0bd61b003f778b23892fe7f9df5
sameeravraj-crypto <sameeravraj@gmail.com>
2025-08-31 11:56:02 +0530

    Add files via upload

---
+++ sum.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b,sum;
+    cout << "Enter number a: ";
+    cin >> a;
+    cout << "Enter number b: ";
+    cin >> b;
+    sum = a+b;
+    cout << "Sum = " << sum << endl;
+    return 0;
+}
