5487089c91a0e782294601f796ee48e08ce66979
sameeravraj-crypto <sameeravraj@gmail.com>
2025-08-31 12:08:43 +0530

    Add files via upload

---
+++ message-with-name.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string name;
+    cout << "Enter your name: ";
+    cin >> name;
+    cout << "Namah Shivaya, " << name << "!" << endl;
+    return 0;
+}
