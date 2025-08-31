738a9c01c31abd06968edf13b7711db1d6913f33
sameeravraj-crypto <sameeravraj@gmail.com>
2025-08-31 20:36:37 +0530

    Add files via upload

---
+++ greatest.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int num1,num2;
+cout << "Enter first number: ";
+cin >> num1;
+cout << "Enter second number: ";
+cin >> num2;
+if(num1 > num2)
+cout << num1 <<" is greater";
+else
+cout << num2 <<" is greater";
+return 0;
+}
